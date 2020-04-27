be082dd97635d0633b09c01f9790dbcdff93927a
Alyssia <alyssia0207@gmail.com>
2020-04-27 15:50:43 +0200

    debut transfert

---
+++ resources/views/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+
+  <div id="menu">
+    <div id="cssThemeSwitcher">
+      <button id="defaultTheme" class="cssTheme"
+              onclick="document.getElementById('cssTheme').href='css/themes/default.css';">Thème Default
+      </button>
+      <button id="crackendTheme" class="cssTheme"
+              onclick="document.getElementById('cssTheme').href='css/themes/crackend.css';">Thème Crack'end
+      </button>
+      <button id="phoenixmlTheme" class="cssTheme"
+              onclick="document.getElementById('cssTheme').href='css/themes/phoenixml.css';">Thème PhoeniXML
+      </button>
+      <button id="gistuneTheme" class="cssTheme"
+              onclick="document.getElementById('cssTheme').href='css/themes/gitsune.css';">Thème Gistune
+      </button>
+    </div>
+
+    <div id="pagesMenu">
+      <img id="logo" src="img/logo.png" alt="logo">
+      <a class="menuLink" href="home.php">Accueil</a>
+      <a class="menuLink" href="rankings.php">Classements</a>
+      <a class="menuLink" href="houses.php">Maisons</a>
+      <a class="menuLink" href="rules.php">Règles</a>
+      <a class="menuLink" href="challenges.php">Défis</a>
+      <a class="menuLink" href="login.php">Login</a>
+    </div>
+  </div>
+
+</header>
+
