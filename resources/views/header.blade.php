6beebb8b7a87b6219c57b621094f54d87acf9fe5
houssam <houssam.laghzil@edu.itescia.fr>
2020-04-29 11:48:03 +0200

    historique

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,6 +9,7 @@
       <a class="menuLink" href="/classements">Classements</a>
       <a class="menuLink" href="/maisons">Maisons</a>
       <a class="menuLink" href="/regles">Règles</a>
+        <a class="menuLink" href="/historique">historique</a>
       <a class="menuLink" href="/challenges">Défis</a>
       <a class="menuLink" href="/login">Login</a>
         <a class="menuLink" href="/utilisateur">User</a>
