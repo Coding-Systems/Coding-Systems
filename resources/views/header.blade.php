ce4a87a7da2dd6878a1cb5f24c4af42a2b50782d
Alyssia <alyssia0207@gmail.com>
2020-04-30 14:26:09 +0200

    organisation Header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,13 +5,13 @@
 
     <div id="pagesMenu">
       <img id="logoHeader" src="img/logo.png" alt="logo">
-      <a class="menuLink" href="/">Accueil</a>
-      <a class="menuLink" href="/classements">Classements</a>
-      <a class="menuLink" href="/maisons">Maisons</a>
-      <a class="menuLink" href="/regles">Règles</a>
+        <a class="menuLink" href="/">Accueil</a>
+        <a class="menuLink" href="/classements">Classements</a>
         <a class="menuLink" href="/historique">Historique</a>
-      <a class="menuLink" href="/challenges">Défis</a>
-      <a class="menuLink" href="/login">Login</a>
+        <a class="menuLink" href="/maisons">Maisons</a>
+        <a class="menuLink" href="/regles">Règles</a>
+        <a class="menuLink" href="/challenges">Défis</a>
+        <a class="menuLink" href="/login">Login</a>
         <a class="menuLink" href="/utilisateur">User</a>
     </div>
   </div>
