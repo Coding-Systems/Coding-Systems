cf3218403938fd5f0cf420b73f796757dd354da7
Alyssia <alyssia0207@gmail.com>
2020-04-30 17:58:46 +0200

    quizz patron

--- resources/views/quizz.blade.php
+++ resources/views/quizz.blade.php
@@ -28,42 +28,46 @@ $numero = rand(0, 3);// un numero aléatoire de 0 à 3
 
         <p>Répondez à ce quiz pour être répartit dans la maison vous correspondant le mieux.
             <br>Ou laissez le hasard total opérer en appuyant <button>ICI</button>
+            <br>
+            <br>Si vous rencontrez des réponses troll, c'est qu'ils s'agit de réponses neutres, en un peu plus fun :P.
         </p>
 
         <form id="quizzForm">
 
             <h2>Question 1</h2>
             <p>À quel élément tu t'identifie le plus ?</p>
-            <label for="Q1A1">L'eau</label>
             <input type="radio" name="question1" id="Q1A1">
-            <label for="Q1A2">La terre</label>
+            <label for="Q1A1">L'eau</label>
             <input type="radio" name="question1" id="Q1A2">
-            <label for="Q1A3">Le feu</label>
+            <label for="Q1A2">La terre</label>
             <input type="radio" name="question1" id="Q1A3">
-            <label for="Q1A4">L'air</label>
+            <label for="Q1A3">Le feu</label>
             <input type="radio" name="question1" id="Q1A4">
+            <label for="Q1A4">L'air</label>
 
             <h2>Question 2</h2>
-            <p>BLABBALBALABLABLABLABALA ?</p>
-            <label for="Q2A1">BLA</label>
+            <p>Quelle est ta plus grande qualité ?</p>
             <input type="radio" name="question2" id="Q2A1">
-            <label for="Q2A2">BLA</label>
+            <label for="Q2A1">Tu es débrouillard</label>
             <input type="radio" name="question2" id="Q2A2">
-            <label for="Q2A3">BLA</label>
+            <label for="Q2A2">Tu es organisé</label>
             <input type="radio" name="question2" id="Q2A3">
-            <label for="Q2A4">BLA</label>
+            <label for="Q2A3">Tu es créatif</label>
             <input type="radio" name="question2" id="Q2A4">
+            <label for="Q2A4">Toute bien évidemment !</label>
 
             <h2>Question X</h2>
             <p>BLABBALBALABLABLABLABALA ?</p>
-            <label for="QXA1">BLA</label>
             <input type="radio" name="questionX" id="QXA1">
-            <label for="QXA2">BLA</label>
+            <label for="QXA1">BLA</label>
             <input type="radio" name="questionX" id="QXA2">
-            <label for="QXA3">BLA</label>
+            <label for="QXA2">BLA</label>
             <input type="radio" name="questionX" id="QXA3">
-            <label for="QXA4">BLA</label>
+            <label for="QXA3">BLA</label>
             <input type="radio" name="questionX" id="QXA4">
+            <label for="QXA4">BLA</label>
+
+            <br></b><input type="submit" value="Envoyer">
 
         </form>
     </section>
