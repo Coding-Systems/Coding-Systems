4c272a120557a3ba0e20322f7875f819a6ee65cd
Marion <meurantmarion@gmail.com>
2020-05-27 16:42:28 +0200

    display ranking when nothing is selected

--- resources/views/historique.blade.php
+++ resources/views/historique.blade.php
@@ -170,6 +170,37 @@
     echo "Il n'y a encore rien à afficher ici !";
 }
 }
+else {
+     $rank=0;
+        $mvt_points = DB::table('mvt_points')
+        ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+        ->join ('houses', 'users.house_id', '=', 'houses.id')
+        ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
+        ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
+        ->orderBy ('mvt_points.created_at')
+        ->get();
+        foreach ($mvt_points as $user) {
+            $rank++;
+            if($user->hname=='Crackend'){
+                echo '<img class="houseIcon" src="img/logoCrackend.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='PhoeniXML'){
+                echo '<img class="houseIcon" src="img/logoPhoenixml.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='Gitsune'){
+                echo '<img class="houseIcon" src="img/logoGitsune.png" alt="logo de la maison"> ';
+            }
+            else {
+                echo '<img id="logoHeader" src="img/logo.png" alt="logo">';
+            }
+            echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name, " : ", $user->label, " [", $user->tname, "]" ; 
+            echo '<br/>';
+            if(intdiv(sizeof($mvt_points),2)==$rank){
+                echo "</p>";
+                echo "<p>";
+            }
+        }
+}
                 
             ?>
         </p>
