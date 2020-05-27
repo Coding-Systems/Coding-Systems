ee197daae31848d337e70df0569f0da2869d23f7
Marion <meurantmarion@gmail.com>
2020-05-27 13:49:59 +0200

    History DB request

--- resources/views/historique.blade.php
+++ resources/views/historique.blade.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
+<?php 
+    use Illuminate\Support\Facades\DB;
+     ?>
 
 <head>
     <meta charset="UTF-8">
@@ -48,7 +51,6 @@
 
          <p>
             <?php
-                use Illuminate\Support\Facades\DB;
                 $rank=0;
                 $mvt_points = DB::table('mvt_points')
                 ->join ('users', 'mvt_points.users_id', '=', 'users.id')
@@ -71,6 +73,70 @@
                     else {
                         echo '<img id="logoHeader" src="img/logo.png" alt="logo">';
                     }
+                    echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name, " : ", $user->label, " [", $user->tname, "]" ; 
+                    echo '<br/>';
+                    if(intdiv(sizeof($mvt_points),2)==$rank){
+                        echo "</p>";
+                        echo "<p>";
+                    }
+                }
+
+
+                $rank=0;
+                $mvt_points = DB::table('mvt_points')
+                ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+                ->join ('houses', 'users.house_id', '=', 'houses.id')
+                ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
+                ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
+                ->where ('houses.name', 'Gitsune')
+                ->orderBy ('mvt_points.created_at')
+                ->get();
+                foreach ($mvt_points as $user) {
+                    $rank++;
+                    echo '<img class="houseIcon" src="img/logoGitsune.png" alt="logo de la maison">';
+                    echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name, " : ", $user->label, " [", $user->tname, "]" ; 
+                    echo '<br/>';
+                    if(intdiv(sizeof($mvt_points),2)==$rank){
+                        echo "</p>";
+                        echo "<p>";
+                    }
+                }
+
+
+
+                $rank=0;
+                $mvt_points = DB::table('mvt_points')
+                ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+                ->join ('houses', 'users.house_id', '=', 'houses.id')
+                ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
+                ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
+                ->where ('houses.name', 'Crackend')
+                ->orderBy ('mvt_points.created_at')
+                ->get();
+                foreach ($mvt_points as $user) {
+                    $rank++;
+                    echo '<img class="houseIcon" src="img/logoCrackend.png" alt="logo de la maison">';
+                    echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name, " : ", $user->label, " [", $user->tname, "]" ; 
+                    echo '<br/>';
+                    if(intdiv(sizeof($mvt_points),2)==$rank){
+                        echo "</p>";
+                        echo "<p>";
+                    }
+                }
+
+
+                $rank=0;
+                $mvt_points = DB::table('mvt_points')
+                ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+                ->join ('houses', 'users.house_id', '=', 'houses.id')
+                ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
+                ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
+                ->where ('houses.name', 'phoeniXML')
+                ->orderBy ('mvt_points.created_at')
+                ->get();
+                foreach ($mvt_points as $user) {
+                    $rank++;
+                    echo '<img class="houseIcon" src="img/logoPhoenixml.png" alt="logo de la maison">';
                     echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name, " : ", $user->label, " [", $user->tname, "]" ; 
                     echo '<br/>';
                     if(intdiv(sizeof($mvt_points),2)==$rank){
