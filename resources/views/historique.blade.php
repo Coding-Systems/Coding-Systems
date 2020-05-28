c3124ac4f04224e72442e67fe0cb954fac4c34f3
Marion <meurantmarion@gmail.com>
2020-05-28 10:37:06 +0200

    Change defi request to display names

--- resources/views/historique.blade.php
+++ resources/views/historique.blade.php
@@ -164,46 +164,72 @@
     }elseif($_POST['histo']=="defis"){
         $rank=0;
         $mvt_points = DB::table('mvt_points')
+        ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+        ->join ('houses', 'users.house_id', '=', 'houses.id')
         ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
-        ->select ('type_points.name AS tname', 'mvt_points.*')
+        ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
         ->where ('type_points.type', 'defi')
         ->orderBy ('mvt_points.created_at')
         ->get();
-        $count = is_null($mvt_points);
         if ($mvt_points->isEmpty()) {
             echo "Il n'y a encore rien à afficher ici !";  
         }
         foreach ($mvt_points as $user) {
             $rank++;
-            echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->tname; 
-            echo '<br/>';
+            if($user->hname=='Crackend'){
+                echo '<img class="houseIcon" src="img/logoCrackend.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='PhoeniXML'){
+                echo '<img class="houseIcon" src="img/logoPhoenixml.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='Gitsune'){
+                echo '<img class="houseIcon" src="img/logoGitsune.png" alt="logo de la maison"> ';
+            }
+            else {
+                echo '<img id="logoHeader" src="img/logo.png" alt="logo">';
+            }
+            echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name," : ", $user->label, " [",$user->tname,"]";             echo '<br/>';
             if(intdiv(sizeof($mvt_points),2)==$rank){
                 echo "</p>";
                 echo "<p>";
             }
         }
+      
     }elseif($_POST['histo']=="events"){
-    $rank=0;
+        $rank=0;
         $mvt_points = DB::table('mvt_points')
+        ->join ('users', 'mvt_points.users_id', '=', 'users.id')
+        ->join ('houses', 'users.house_id', '=', 'houses.id')
         ->join ('type_points', 'mvt_points.type_point_id', '=', 'type_points.id')
-        ->select ('type_points.name AS tname', 'mvt_points.*')
+        ->select ('users.first_name', 'houses.name AS hname', 'type_points.name AS tname', 'mvt_points.*')
         ->where ('type_points.type', 'events')
         ->orderBy ('mvt_points.created_at')
         ->get();
         if ($mvt_points->isEmpty()) {
             echo "Il n'y a encore rien à afficher ici !";  
         }
-        else {
         foreach ($mvt_points as $user) {
             $rank++;
-            echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->tname; 
-            echo '<br/>';
+            if($user->hname=='Crackend'){
+                echo '<img class="houseIcon" src="img/logoCrackend.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='PhoeniXML'){
+                echo '<img class="houseIcon" src="img/logoPhoenixml.png" alt="logo de la maison">';
+            }
+            else if ($user->hname=='Gitsune'){
+                echo '<img class="houseIcon" src="img/logoGitsune.png" alt="logo de la maison"> ';
+            }
+            else {
+                echo '<img id="logoHeader" src="img/logo.png" alt="logo">';
+            }
+            echo " [", date('d/m H:i', strtotime($user->created_at)), "] ", $user->first_name," : ", $user->label, " [",$user->tname,"]";             echo '<br/>';
             if(intdiv(sizeof($mvt_points),2)==$rank){
                 echo "</p>";
                 echo "<p>";
             }
         }
-        }
+      
+        
 }
 }
 else {
