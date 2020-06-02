652c70c0d2ad7bb5f5d83c2fa0ca4d97a90ad9be
Alyssia <alyssia0207@gmail.com>
2020-06-02 19:18:10 +0200

    forms dones

--- resources/views/challenges.blade.php
+++ resources/views/challenges.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 
+<?php $idUser=1;
+?>
+
 <head>
   <meta charset="UTF-8">
   <title>Coding house</title>
@@ -22,7 +25,7 @@
 
 <?php
 
-    $idUser=5;
+
 
 $userType = DB::select('SELECT statut
                     FROM users
@@ -37,7 +40,7 @@ if($userType[0]->statut=='student'){
         FROM type_points
         WHERE type="defi"');
 
-    echo '<form name="newDefiForm" id="newDefiForm" method="post"> '. csrf_field() ; //echo '{{ csrf_field() }}';
+    echo '<form name="newDefiForm" id="newDefiForm" method="post"> '.csrf_field() ; //echo '{{ csrf_field() }}';
 
     echo '<h3>Choisissez votre défis :</h3>';
 
@@ -87,11 +90,6 @@ if($userType[0]->statut=='student'){
     echo '</form>';
     }
 
-    echo '<h2>Demandes en attente :</h2>';
-
-?>
-
-<?php
 if(isset($_POST['OpponentId']) && isset($_POST['arbiterId']) &&isset($_POST['defiTypeId'])){
     //OpponentId arbiterId defiTypeId
 
@@ -101,47 +99,146 @@ if(isset($_POST['OpponentId']) && isset($_POST['arbiterId']) &&isset($_POST['def
 
     echo "<p>";
     if ($listDefis[0]->total==0){
-        echo "Demande de défi et d'arbitrage envoyés";
+
+        $houseOponent = DB::select('SELECT house_id
+            FROM users
+            WHERE id= :id', ['id' => $_POST['OpponentId']]);
+
+        $houseArbiter = DB::select('SELECT house_id
+            FROM users
+            WHERE id= :id', ['id' => $_POST['arbiterId']]);
+
+        if($houseArbiter[0]->house_id != $houseOponent[0]->house_id){
+            DB::table('defis')->insert(
+                array(
+                    'challenger_id' => "$idUser",
+                    'target_id' => $_POST['OpponentId'],
+                    'arbiter_id' => $_POST['arbiterId'],
+                    'type_id' => $_POST['defiTypeId']
+                )
+            );
+            echo "<p>Demande de défi et d'arbitrage envoyés</p>";
+        }
+        else{
+            echo "<p>Échec.</br> L'adversaire et l'arbitre doivent être dans des maisons différentes.</p>";
+        }
     }
     else {
         echo"Vous avez déja un défi en attente de reponse. Annulez-le ou finissez-le avant de défier d'autres personnes.";
     }
     echo "</p>";
-
-
-
 }
-
 ?>
 
+<?php
+    echo '<h2>Demandes en attente :</h2>';
 
-<form class="tableDefi">
-    <table>
-        <tbody>
-        <tr>
-            <th colspan="2">Demandes de défis</th>
-        </tr>
-        <tr>
-            <td>Smash | Vous VS Houssam | Arbitre : Marion</td>
-            <td><input type="button" value="Accepter"><input type="button" value="Refuser"></td>
-        </tr>
-        <tr>
-            <th colspan="2">Demandes d'arbitrage</th>
-        </tr>
-        <tr>
-            <td>Smash | Hugo VS Houssam | Arbitre : Vous</td>
-            <td><input type="button" value="Accepter"><input type="button" value="Refuser"></td>
-        </tr>
-        <tr>
-            <th colspan="2">Demandes envoyées</th>
-        </tr>
-        <tr>
-            <td>Smash | Vous VS Houssam | Arbitre : Alyssia</td>
-            <td><input type="button" value="Annuler"></td>
-        </tr>
-        </tbody>
-    </table>
-</form>
+    echo '<h3>Propositions de défis</h3>';
+echo "<p>Vous pouvez accepter ou non une proposition de défi.</p>";
+
+
+    $challengesInvitation = DB::select('SELECT type_points.name AS type, creator.first_name as cName, arbiter.first_name AS aName, defis.id AS idDefi
+                FROM defis
+                LEFT JOIN users AS creator
+                    ON creator.id = defis.challenger_id
+                LEFT JOIN users AS arbiter
+                    ON arbiter.id = arbiter_id
+                LEFT JOIN type_points
+                    ON type_points.id = type_id
+                WHERE target_id = :id
+                    AND is_accepted IS NULL
+                ', ['id' => $idUser]);
+
+    if(isset($challengesInvitation[0])){
+        echo '<form name="acceptDefi" id="AcceptDefiForm" method="post"> '.csrf_field() ;
+        echo '<select required="required" name="proposedDefid" size="3">';
+
+        foreach ($challengesInvitation as $challenge){
+            echo '<option value="'.$challenge->idDefi.'">'.$challenge->type.' | Vous VS '.$challenge->cName.' | Arbitre : '.$challenge->aName.'</option>';
+        };
+        echo '</select>
+            <br><input type="radio" name="acceptDefi" id=acceptDefi checked="checked"> <label for="acceptDefi">Accepter</label>
+            <br><input type="radio" name="acceptDefi" id="deniedDefi"> <label for="deniedDefi">Refuser</label>
+            <br><input type="submit" value="valider">
+
+            </form>';
+    }
+    else {
+        echo "Aucune proposition de défis en attente.";
+    }
+
+
+
+echo "<h3>Demandes d'arbitrage</h3>";
+echo "<p>Vous pouvez accepter ou non une demande d'arbitrage.</p>";
+
+    $arbitorInvitation = DB::select('SELECT type_points.name AS type, creator.first_name as cName, opponent.first_name AS oName, defis.id AS idDefi
+                FROM defis
+                LEFT JOIN users AS creator
+                    ON creator.id = defis.challenger_id
+                LEFT JOIN users AS opponent
+                    ON opponent.id = target_id
+                LEFT JOIN type_points
+                    ON type_points.id = type_id
+                WHERE arbiter_id = :id
+                    AND is_accepted =True
+                ', ['id' => $idUser]);
+
+    if (isset($arbitorInvitation[0])){
+        echo '<form name="acceptArbiform" id="acceptArbiform" method="post"> '.csrf_field() ;
+        echo '<select required="required" name="proposedArbi" size="3">';
+
+        foreach ($arbitorInvitation as $challenge){
+            echo '<option value="'.$challenge->idDefi.'">'.$challenge->type.' | '.$challenge->oName.' VS '.$challenge->cName.' | Arbitre : Vous </option>';
+        };
+        echo '</select>
+            <br><input type="radio" name="acceptArbiRadio" id=acceptArbi checked="checked"> <label for="acceptArbi">Accepter</label>
+            <br><input type="radio" name="acceptArbiRadio" id="deniedArbi"> <label for="deniedArbi">Refuser</label>
+            <br><input type="submit" value="valider">
+
+            </form>';
+    }
+    else {
+        echo "Aucune demande d'arbitrage en attente.";
+    }
+
+
+
+echo "<h3>Demandes envoyées</h3>";
+
+echo "<p>Vous pouvez annuler une ancienne demande de défi pour en lancer une nouvelle.</p>";
+
+$createdDefis = DB::select('SELECT type_points.name AS type, arbitor.first_name as aName, opponent.first_name AS oName, defis.id AS idDefi
+                FROM defis
+                LEFT JOIN users AS arbitor
+                    ON arbitor.id = defis.arbiter_id
+                LEFT JOIN users AS opponent
+                    ON opponent.id = target_id
+                LEFT JOIN type_points
+                    ON type_points.id = type_id
+                WHERE challenger_id = :id
+                ', ['id' => $idUser]);
+
+if(isset($createdDefis[0])){
+    echo '<form name="DeleteDdefi" id="DeleteDdefi" method="post"> '.csrf_field() ;
+    echo '<select required="required" name="CreateDdefi" size="3">';
+
+    foreach ($createdDefis as $challenge){
+
+        echo '<option value="'.$challenge->idDefi.'">'.$challenge->type.' | Vous VS '.$challenge->oName.' | Arbitre : '.$challenge->aName.'</option>';
+        echo '</select>
+            <br><input type="submit" value="Annuler">
+
+            </form>';
+    }
+}
+else {
+    echo "Ancun défi en attente.";
+}
+
+
+
+?>
 
 @include('footer')
 
