ffe91c3c1bf78b490f937ada5ba05be0565cd58b
Marion <meurantmarion@gmail.com>
2020-06-02 10:06:53 +0200

    Add points in houses too

--- resources/views/addingPoints.blade.php
+++ resources/views/addingPoints.blade.php
@@ -30,6 +30,7 @@
 
 <?php
 $studentList = DB::table('users')
+->where('statut', 'student')
 ->get();
 
 $challengeList = DB::table('type_points')
@@ -80,24 +81,43 @@ if(isset($_POST['envoi'])){
       ->where('id', $student_id)
       ->get();
       
-      $type_Pts = DB::table('type_points')
+      $type_pts = DB::table('type_points')
       ->select ('type_points.type', 'type_points.id')
       ->where ('id', $challenge_id )
       ->get ();
 
-      foreach ($student_points as $add) {
-            $total_points = $add->total_pts;
-            echo $total_points;
+      $house = DB::table('users')
+      ->select('users.house_id', 'users.id')
+      ->where('id', $student_id)
+      ->get();
+
+      foreach ($house as $house){
+            $house_id = $house->house_id;
+      }
+
+      $house_pts = DB::table('houses')
+      ->select('houses.total_pts', 'houses.total_pts_po')
+      ->where('id', $house_id)
+      ->get();
+
+      foreach ($house_pts as $add_house_pts){
+            $house_total_pts = $add_house_pts->total_pts;
+      }
+
+      foreach ($house_pts as $add_house_pts_po){
+            $house_total_pts_po = $add_house_pts->total_pts_po;
+      }
+
+      foreach ($student_points as $add){
+            $total_pts = $add->total_pts;
       }
 
       foreach ($student_points as $add){
             $total_pts_po = $add->total_pts_po;
-            echo $total_pts_po;
       }
       
-      foreach ($type_Pts as $type) {
+      foreach ($type_pts as $type) {
             $type_select = $type->type;
-            echo $type_select;
       }
 
      if ($type_select=="PO"){
@@ -114,36 +134,33 @@ if(isset($_POST['envoi'])){
             ->where("id", $student_id)
             ->update(
                   array(
-                        'total_pts'=> "$nbr_points"+"$total_points"
+                        'total_pts'=> "$nbr_points"+"$total_pts"
                   )
                   );
 
-                  DB::table('users')
-                  ->where("id", $student_id)
-                  ->update(
-                        array(
-                              'total_pts_po'=> "$nbr_points"+"$total_pts_po"
-                        )
-                        );
-            }
-
-            
-     else if($type_select=="events") {
-      array(
-            'label' => "$nbr_points",
-            'users_id' => "$student_id",
-            'type_point_id' => "$challenge_id",
-            'created_at' => "$date"
-      );
-
-      DB::table('users')
-      ->where("id", $student_id)
-      ->update(
-            array(
-                  'total_pts'=> "$nbr_points"+"$total_points"
-            )
+            DB::table('users')
+            ->where("id", $student_id)
+            ->update(
+                  array(
+                        'total_pts_po'=> "$nbr_points"+"$total_pts_po"
+                  )
             );
-      }
+
+            DB::table('houses')
+            ->where("id", $house_id)
+            ->update(
+                  array(
+                        'total_pts'=>"$nbr_points"+"$house_total_pts"
+                  )
+                  );
+            DB::table('houses')
+            ->where("id", $house_id)
+            ->update(
+                  array(
+                        'total_pts_po'=>"$nbr_points"+"$house_total_pts_po"
+                  )
+                  );
+            }
 
       else {
             echo "Une erreur s'est produite. Veuillez réessayer.";
