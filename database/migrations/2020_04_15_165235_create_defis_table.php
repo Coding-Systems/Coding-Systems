5ba13442b5de357e28940cd7d31a6c6699ae0bdb
Alyssia <alyssia0207@gmail.com>
2020-06-02 10:36:28 +0200

    migrations table defi

---
+++ database/migrations/2020_04_15_165235_create_defis_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDefisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('defis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('challenger_id');
+            $table->foreignId('target_id');
+            $table->foreignId('arbiter_id');
+            $table->foreignId('winner_id');
+            $table->foreignId('type_id');
+            $table->timestamps();
+
+            $table->foreign('challenger_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('target_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('arbiter_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('winner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('type_id')->references('id')->on('mvt_points')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('defis');
+    }
+}
