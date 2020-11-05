bd4df768179e86893cced97cc67eff873cf7b92e
Axel Pelerin <arcanynka@gmail.com>
2020-11-05 10:57:20 +0100

    Upgrade 7 to 8: Update namespaces for seeders and factories

--- database/seeders/MvtPointsTableSeeder.php
+++ database/seeders/MvtPointsTableSeeder.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class MvtPointsTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 require 'UsersImportExel.php';
@@ -7,7 +9,7 @@ require 'UsersImportExel.php';
 class UsersTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
