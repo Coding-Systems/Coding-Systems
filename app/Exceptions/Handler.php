540a7673bc9f65a3e3f0c31a68cdaf2b33e839cc
Marion <meurantmarion@gmail.com>
2021-05-02 14:07:58 +0200

    Start good method of 404 page. Should watch a tuto

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('errors.' . '404', [], 404);
+            }
+             
+            if ($exception->getStatusCode() == 500) {
+                return response()->view('errors.' . '500', [], 500);
+            }
+        }
         return parent::render($request, $exception);
     }
 }
