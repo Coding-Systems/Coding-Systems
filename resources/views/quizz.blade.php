b06464f85c0aa09eea6fe6b020e91817d35e73dc
houssam <hlaghzil@gmail.com>
2021-05-03 11:56:20 +0200

    fix css quizz

--- resources/views/quizz.blade.php
+++ resources/views/quizz.blade.php
@@ -14,6 +14,7 @@ $idUser = 3;
     <link rel="stylesheet" href="css/app.css"/>
     <link rel="stylesheet" href="css/all.css"/>
     <link rel="stylesheet" href="css/pages/quizz.scss"/>
+    <link rel="stylesheet" href="css/pages/quizzradio.css"/>
     <link rel="stylesheet" href="css/pages/quizzsass.css"/>
     <link href="resources/js/app.js">
 
@@ -47,7 +48,7 @@ $numero = rand(0, 3);// un numero aléatoire de 0 à 3
 
     </form>
     <br>
-    <br>Si vous rencontrez des réponses troll, c'est qu'ils s'agit de réponses neutres, mais en un peu plus fun :P.
+    <br><p>Si vous rencontrez des réponses troll, c'est qu'ils s'agit de réponses neutres, mais en un peu plus fun :P.
     </p>
 
         <form id="quizzForm" name="quizzForm" method="post"> <!-- PENSER A CHANGER L'ORDRE DE CERTAINES !!!!! -->
@@ -58,9 +59,16 @@ $numero = rand(0, 3);// un numero aléatoire de 0 à 3
                 <p class="quizzPage">{{$question['questionText']}}</p>
                 @foreach($question['answers'] as $answer)
 
-                    <div class="question">
-                        <input type="radio" name="radio{{$loop->parent->index}}" id="Q{{$loop->parent->iteration}}A{{$loop->index}}" value="{{$loop->index}}">
-                        <label for="Q{{$loop->parent->iteration}}A{{$loop->index}}">{{$answer['text']}}</label>
+                    <div class="question container cntr">
+                        <label class="radio btn-radio" for="Q{{$loop->parent->iteration}}A{{$loop->index}}">{{$answer['text']}}
+                            <input class="check" type="radio" name="radio-grp"{{$loop->parent->index}}" id="Q{{$loop->parent->iteration}}A{{$loop->index}}" value="{{$loop->index}}">
+                            <svg width="20px" height="20px" viewBox="0 0 20 20">
+                                <circle cx="10" cy="10" r="9"></circle>
+                                <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
+                                <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
+                            </svg>
+                        </label>
+
                     </div>
 
 
