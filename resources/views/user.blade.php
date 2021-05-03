7f949b6be546ff6de7d6552e69f976dab691a37a
Alyssia <alyssia0207@gmail.com>
2021-05-03 17:21:55 +0200

    change themes exept phoenixml

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,6 +12,7 @@
     @include('cssSwitcher')
     <link rel="stylesheet" href="css/app.css"/>
     <link rel="stylesheet" href="css/all.css"/>
+    <link rel="stylesheet" href="css/pages/user.scss"/>
     <link href="resources/js/app.js">
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
