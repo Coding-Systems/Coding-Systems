187663000c3b042a971d8efb1c7fce72ae844517
Alyssia <alyssia0207@gmail.com>
2021-05-06 09:14:22 +0200

    remove useless br

--- resources/views/quizz.blade.php
+++ resources/views/quizz.blade.php
@@ -74,8 +74,6 @@ $numero = rand(0, 3);// un numero aléatoire de 0 à 3
 
                 @endforeach
             @endforeach
-            <br>
-            <br>
             <input type="submit" value="Envoyer" name="envoi" id="submitButton">
 
         </form>
