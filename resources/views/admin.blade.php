cfef2d8969537b0b01582d9b09d6172a879e9042
houssam <hlaghzil@gmail.com>
2021-05-06 11:34:26 +0200

    split div

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -26,21 +26,22 @@ use App\Providers\CheckLogoLvl;
     <div class="bg bg2"></div>
     <div class="bg bg3"></div>
     <h1>Admin</h1>
+    <div class="buttonUP">
+    <div id="buttonUPA">
 
-    <H2>Lancer la répartition</H2>
-
-    <?php
-    $promoList = DB::select('SELECT id, name
-            FROM promo WHERE is_distributed = false');
-
-    //foreach ($promoList as $promo)
-    ?>
-
-    <h3>Ratio de personnes ayant répondu :</h3>
+        <H2>Lancer la répartition</H2>
 
         <?php
-        foreach ($promoList as $promo)
-            {
+        $promoList = DB::select('SELECT id, name
+            FROM promo WHERE is_distributed = false');
+
+        //foreach ($promoList as $promo)
+        ?>
+
+        <h3>Ratio de personnes ayant répondu :</h3>
+
+        <?php
+        foreach ($promoList as $promo) {
 
             $usersTotal = DB::select('SELECT u.id
         FROM users AS U
@@ -55,52 +56,53 @@ use App\Providers\CheckLogoLvl;
         AND RT.quizz_is_done = 1
         ', ['id' => $promo->id]);
 
-            echo '<p>'.$promo->name." : ".count($usersDone)."/".count($usersTotal).' ont fait le quiz';
+            echo '<p>' . $promo->name . " : " . count($usersDone) . "/" . count($usersTotal) . ' ont fait le quiz';
 
-            }
+        }
         ?>
+        </select>
+
+        <form method="post"> {{ csrf_field() }}
+
+                <select id="promoSelect" name="promoSelect">
+
+                    <?php
+                    foreach ($promoList as $promo) {
+                        echo '<option value="' . $promo->id . '">' . $promo->name . '</option>';
+                    };
+                    ?>
                 </select>
+                <button id="buttonAdmin">Start</button>
+            </div>
 
-    <form method="post"> {{ csrf_field() }}
-        <div>
-            <select id="promoSelect" name="promoSelect">
+        </form>
 
-                <?php
-                foreach ($promoList as $promo) {
-                    echo '<option value="' . $promo->id . '">' . $promo->name . '</option>';
-                };
-                ?>
-            </select>
-            <button id="buttonAdmin">Start</button>
+        <?php
+        const DistributionServiceProvider = 'app\Providers\DistributionServiceProvider.php';
+        $distrib = new DistributionServiceProvider;
+
+        if (isset($_POST['promoSelect'])) {
+            $distrib->index($_POST['promoSelect']);
+        }
+        ?>
+        <div id="buttonUPB">
+            <h2 id="h2logos">Lancer l'amélioration des logos</h2>
+
+            <form method="post"> {{ csrf_field() }}
+                <button id="buttonAdminLogoLvl" name="startCheckLogolvl">Start</button>
+            </form>
         </div>
 
-    </form>
+        <?php
 
-    <?php
-    const DistributionServiceProvider = 'app\Providers\DistributionServiceProvider.php';
-    $distrib = new DistributionServiceProvider;
+        const CheckLogoLvl = 'app\Providers\CheckLogoLvl.php';
+        $checkLogo = new CheckLogoLvl;
 
-    if (isset($_POST['promoSelect'])) {
-        $distrib->index($_POST['promoSelect']);
-    }
-    ?>
-
-    <h2>Lancer l'amélioration des logos</h2>
-
-    <form method="post"> {{ csrf_field() }}
-        <button id="buttonAdminLogoLvl" name="startCheckLogolvl">Start</button>
-    </form>
-
-    <?php
-
-    const CheckLogoLvl = 'app\Providers\CheckLogoLvl.php';
-    $checkLogo = new CheckLogoLvl;
-
-    if (isset($_POST['startCheckLogolvl'])) {
-        $checkLogo->startCheck();
-    }
-    ?>
-
+        if (isset($_POST['startCheckLogolvl'])) {
+            $checkLogo->startCheck();
+        }
+        ?>
+    </div>
     <h2>Ajouter une promotion</h2>
 
     <form method="post"> {{ csrf_field() }}
@@ -112,11 +114,10 @@ use App\Providers\CheckLogoLvl;
     </form>
 
     <?php
-     if (isset($_POST['addPromo'])) {
+    if (isset($_POST['addPromo'])) {
         if (DB::table('promo')->where('name', '=', $_POST['namePromo'])->exists()) {
             echo 'La promo existe déjà.';
-        }
-        else {
+        } else {
             DB::table('promo')->insert(
                 array(
                     'name' => $_POST['namePromo']
@@ -127,35 +128,34 @@ use App\Providers\CheckLogoLvl;
     }
     ?>
 
-<h2>Ajouter un défi</h2>
+    <h2>Ajouter un défi</h2>
 
-<form method="post"> {{ csrf_field() }}
-    <p>Nom du challenge</p>
-    <input required="required" name="nameChallenge"/>
-    <p>Nombre de points</p>
-    <div>
-        <input id="inputAdminChallenge" required="required" name="numberPointsChallenge" />
-        <button id="buttonAdminChallenge" name="addChallenge">Ajouter</button>
-    </div>
+    <form method="post"> {{ csrf_field() }}
+        <p>Nom du challenge</p>
+        <input required="required" name="nameChallenge"/>
+        <p>Nombre de points</p>
+        <div>
+            <input id="inputAdminChallenge" required="required" name="numberPointsChallenge"/>
+            <button id="buttonAdminChallenge" name="addChallenge">Ajouter</button>
+        </div>
 
-</form>
+    </form>
 
-<?php
- if (isset($_POST['addChallenge'])) {
-    if (DB::table('defis_type')->where('label', '=', $_POST['nameChallenge'])->exists()) {
-        echo 'Le défi existe déjà.';
+    <?php
+    if (isset($_POST['addChallenge'])) {
+        if (DB::table('defis_type')->where('label', '=', $_POST['nameChallenge'])->exists()) {
+            echo 'Le défi existe déjà.';
+        } else {
+            DB::table('defis_type')->insert(
+                array(
+                    'label' => $_POST['nameChallenge'],
+                    'number_of_points' => $_POST['numberPointsChallenge']
+                )
+            );
+            echo 'Le défi a été créé.';
+        }
     }
-    else {
-        DB::table('defis_type')->insert(
-            array(
-                'label' => $_POST['nameChallenge'],
-                'number_of_points' => $_POST['numberPointsChallenge']
-            )
-        );
-        echo 'Le défi a été créé.';
-    }
-}
-?>
+    ?>
 
 
 </section>
